c79b37f4c7e98293da397c8fcd39b364ecbb5aa4
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2020-03-02 02:36:17 +0600

    test some minor isssue

--- Offline1/script.sh
+++ Offline1/script.sh
@@ -21,3 +21,28 @@ input=`tr "\n" "-" < $file`
 var1=$(echo $input | cut -f1 -d-)
 var2=$(echo $input | cut -f2 -d-)
 var3=$(echo $input | cut -f3 -d-)
+
+
+
+#trying out stuff
+
+if [ $var1 = "begin" ]; then
+    echo begin
+elif [ $var1 = "end" ]; then
+    echo end
+else 
+    echo "invalid first input"
+fi
+
+
+#head -$var2 $file | grep -i $var3
+
+
+for i in `cd ./working_dir/ | ls -R`
+do
+    chmod a+rwx $i
+    head -$var2 $i | grep -i $var3
+done
+
+
+#TODO: for each valid file in the directory read first/last n lines and search the word containing files and put them in a output directory
