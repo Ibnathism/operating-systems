ff6d5f36014cbe8e09b71ec6dc0c45edfdc3017c
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2020-03-03 13:54:19 +0600

    go back upto task5

--- Offline1/script.sh
+++ Offline1/script.sh
@@ -30,31 +30,17 @@ var2=$(echo $input | cut -f2 -d-)
 var3=$(echo $input | cut -f3 -d-)
 
 #task 5
-send_to_output_dir(){
-    cp $1 "$2/../../output_dir/"
-    
-}
+
 
 
 
 check_readable_files()
 {
-    directory=$1
-
-    global="$global/$directory"
-    depthCount=`expr $depthCount + 1`
-    #echo $global
     cd ./$directory/
-
     for file in `ls`;do
-
         if [[ -d $file ]]; then
             check_readable_files $file
             cd ..
-            #echo $global
-            #global=`cut -d / -f 2 < ..$global/`
-            oldPwd=$OLDPWD
-            depthCount=`expr $depthCount - 1`
         elif [[ -f $file ]]; then
             if [ $var1 = "begin" ]; then
                 temp=`head -$var2 $file | grep -i $var3`
@@ -63,28 +49,14 @@ check_readable_files()
             else 
                 echo "invalid first input"
             fi
-
             if [[ -n $temp ]];then
-                #echo "$file has $temp"
-                #rename_file $file $global
-                #echo $oldPwd
-                echo "$file transferring from $oldPwd"
-                send_to_output_dir $file $oldPwd
-                #send_to_output_dir $file $depthCount
-                depthCount=1
+                echo "$file has $temp"
             fi
-
         fi
-        #echo "$file"
-        
     done
 
 }
 
-mkdir output_dir
-
-depthCount=1
-global=""
 check_readable_files $dir
 
 #TODO: put them in a output directory
